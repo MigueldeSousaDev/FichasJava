f5bda98e9ca66bd475aed2d90cc06fa24525fd6c
Miguel de Sousa <miguelsousa355@gmail.com>
2026-05-11 00:54:26 +0100

    Exercício 6 comentado e resolvido.

--- PrimeiroProjeto/src/FichaPratica7/BibliotecaDeFicheiros.java
+++ PrimeiroProjeto/src/FichaPratica7/BibliotecaDeFicheiros.java
@@ -3,6 +3,7 @@ package FichaPratica7;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 
@@ -10,19 +11,19 @@ public class BibliotecaDeFicheiros {
 
 
     //Ex-1
-    public static void imprimirFicheiroNaConsola (String caminhoFicheiro) throws FileNotFoundException {
+    public static void imprimirFicheiroNaConsola(String caminhoFicheiro) throws FileNotFoundException {
 
         File ficheiro = new File(caminhoFicheiro);
         Scanner meuScannerFic = new Scanner(ficheiro);
 
-        while(meuScannerFic.hasNextLine()){
+        while (meuScannerFic.hasNextLine()) {
             String linha = meuScannerFic.nextLine();
             System.out.println(linha);
         }
     }
 
     //Ex-2
-    public static void criarFicheiroEscrever () throws FileNotFoundException {
+    public static void criarFicheiroEscrever() throws FileNotFoundException {
 
         String fileContent = "Hello! Adoro programar em Java!";
 
@@ -40,13 +41,13 @@ public class BibliotecaDeFicheiros {
     }
 
     //Ex-3
-    public static void lerECopiarFicheiro () throws FileNotFoundException {
+    public static void lerECopiarFicheiro() throws FileNotFoundException {
 
         // Abrir Ficheiro
         File ficheiroOriginal = new File("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_03.txt");
 
         // Criar Ficheiro Cópia
-        File ficheiroCopia = new File ("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_03_copia.txt");
+        File ficheiroCopia = new File("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_03_copia.txt");
 
 
         // Ler ou Escrever ficheiro (scanner vs printwriter)
@@ -69,20 +70,19 @@ public class BibliotecaDeFicheiros {
 
     public static void lerEImprimirIDE() throws FileNotFoundException {
 
-            // Abrir OEM
-            File csvFileOriginal = new File("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_04.csv");
+        // Abrir OEM
+        File csvFileOriginal = new File("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_04.csv");
 
-            // Importar scanner
+        // Importar scanner
 
-            Scanner scannertuga = new Scanner(csvFileOriginal);
+        Scanner scannertuga = new Scanner(csvFileOriginal);
 
-            while (scannertuga.hasNextLine()) {
-                String linha = scannertuga.nextLine();
+        while (scannertuga.hasNextLine()) {
+            String linha = scannertuga.nextLine();
 
-                System.out.println(linha);
-            }
+            System.out.println(linha);
         }
-
+    }
 
 
     // --------------------------------------------------------------------------
@@ -91,7 +91,7 @@ public class BibliotecaDeFicheiros {
     public static void lerStringNumeros() throws FileNotFoundException {
 
         // 1. Abrir ficheiro e ler com Scanner
-        // 2. Split dos numeros (verificr se necessário integer.parse
+        // 2. Split dos numeros (verificar se necessário integer.parse)
 
         /**@linha - Recebe toda a linha do ficheiro (no ciclo)
          * @itemsLinha - Criamos como Vetor de Strings vazio e atribuimos o conteúdo de "linha", mas separado por espaço. O espaço é usado como delimitador do vetor.
@@ -123,7 +123,53 @@ public class BibliotecaDeFicheiros {
     }
 
 
+//--------------------------------------------------------------------------------------
+    //--- Ex.6 (Encontra num ficheiro delimitado por 2 campos por 1 "," | 1º Texto | 2º Número (2digitos) >
+    // Problema: Encontra o primeiro maior valor e guarda numa variável. Se existir mais que uma pessoa com a mesma idade não dá print delas.
+    // Mas encontra o maior valor que é o propósito do exercício.
 
+    public static void imprimirPessoaMaisVelha() throws FileNotFoundException {
+
+        // Abrir Ficheiro
+        File AbrirFicheiro = new File("C:\\Users\\User\\Desktop\\OneDrive - CESAE\\Ambiente de Trabalho\\Professor Vitor\\FichasJava\\PrimeiroProjeto\\src\\Ficheiros\\FichaPratica07\\exercicio_06.txt");
+
+        // Para ler o Ficheiro
+        Scanner scannerDeLinha = new Scanner(AbrirFicheiro);
+
+        // Este código faz com que atribua o nome ao vetor [0] de cada linha. Não precisamos de Matriz porque ele analisa linha a linha e guarda a maior variável, assim como dá print
+        // por cada linha. Para a idade, usa a posição [1] do Vetor.
+        // É possível refazer código com > int menorIdade = Integer.MIN_VALUE;    > Isto retira a necessidade do boolean para o 1º valor
+
+        int idade = 0;
+        int maiorIdade = 0 ;
+        String nomeMaisVelho = "";
+        boolean primeiro = true;
+
+        while (scannerDeLinha.hasNextLine()) {
+            String linha = scannerDeLinha.nextLine();
+            String[] itemsLinha = linha.split(",");
+
+            String nome = itemsLinha[0];
+            idade = Integer.parseInt(itemsLinha[1]);
+
+            if (primeiro) {
+                maiorIdade = idade;
+                nomeMaisVelho = nome;
+                primeiro = false;
+            }
+            else if (idade > maiorIdade) {
+                maiorIdade = idade;
+                nomeMaisVelho = nome;
+                }
+            //Opcional print de tudo (código feito para isso)
+            System.out.println("Nome: " + nome + "| Idade: " + idade);
+        }
+        System.out.println("\n A pessoa mais velha é:\nNome: " + nomeMaisVelho + "| Idade: " +  maiorIdade);
+    }
 }
+
+
+
+
 
 
--- PrimeiroProjeto/src/FichaPratica7/Exercicio6.java
+++ PrimeiroProjeto/src/FichaPratica7/Exercicio6.java
@@ -2,11 +2,13 @@ package FichaPratica7;
 
 import java.io.FileNotFoundException;
 
+import static FichaPratica7.BibliotecaDeFicheiros.imprimirPessoaMaisVelha;
+
 public class Exercicio6 {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-
+    imprimirPessoaMaisVelha();
 
     }
 }
